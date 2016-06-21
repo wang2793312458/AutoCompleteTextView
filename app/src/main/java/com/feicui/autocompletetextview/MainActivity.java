b0ad3162b501d31daa412a9c79bdc033fca8c435
wz2793312458 <wz2793312458@163.com>
2016-06-21 20:43:44 +0800

    Initial commit

---
+++ app/src/main/java/com/feicui/autocompletetextview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.feicui.autocompletetextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView atv_content;
+    private MultiAutoCompleteTextView matv_content;
+
+    private static final String[] data = new String[]{
+            "小猪猪", "小狗狗", "小鸡鸡", "小猫猫", "小咪咪"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        atv_content= (AutoCompleteTextView) findViewById(R.id.atv_content);
+        matv_content= (MultiAutoCompleteTextView) findViewById(R.id.matv_content);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_dropdown_item_1line,data);
+        atv_content.setAdapter(adapter);
+
+        ArrayAdapter<String>adapter1=new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_dropdown_item_1line, data);
+
+        matv_content.setAdapter(adapter);
+        matv_content.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+    }
+}
